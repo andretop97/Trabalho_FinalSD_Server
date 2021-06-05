911cf5ec1b6a59b6994a90acde9d250456eb59fb
Andre Luiz Branco Barbosa Araujo <andreluizpsn975@hotmail.com>
2021-06-05 01:35:41 -0300

    cors politic

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,8 @@ import express from 'express';
 import { router } from './routes';
 import { connection } from './database/MongoDB/Connection';
 import cors from 'cors';
-
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 
 connection();
 
